141c80eb7a158df102871caac401468852546cd2
R1T1KK <161676725+R1T1KK@users.noreply.github.com>
2026-02-25 11:13:25 +0530

    Modify User table and add Income tables
    Added default value for created_at and created new Income and Income1 tables with sample data.

--- Exp7.sql
+++ Exp7.sql
@@ -22,9 +22,34 @@ CREATE TABLE User (
     name VARCHAR(100),
     email VARCHAR(100),
     password VARCHAR(100) CHECK (CHAR_LENGTH(password) >= 8),
-    created_at VARCHAR(50)
+    created_at VARCHAR(50) default '2026-02-25'
 );
 INSERT INTO User (user_id, name, email, password, created_at) VALUES
 (1, 'Hadeen', 'hadeen@gmail.com', 'pass1234', '2026-02-25'),
 (2, 'Ali', 'ali@gmail.com', 'ali@2026', '2026-02-24'),
 (3, 'Sara', 'sara@gmail.com', 'sara', '2026-02-23');
+
+CREATE TABLE Income (
+    income_id INT PRIMARY KEY,
+    user_id INT,
+    amount DOUBLE,
+    source VARCHAR(100),
+    income_date INT
+);
+
+CREATE TABLE Income1 (
+    income_id INT ,
+    salary int PRIMARY KEY,
+    username varchar(100),
+    
+    foreign key (income_id) references Income(income_id)
+    
+);
+
+INSERT INTO Income1 (income_id, salary, username) VALUES
+(1, 50000, 'Hadeen'),
+(2, 30000, 'Ali'),
+(3, 45000, 'Sara');
+
+select * from budget1;
+select * from user
